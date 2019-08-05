dca61edff4b613ea753b7717c0c06948a35764ef
Ian Kennedy <iankennedy@D-10-19-245-168.dhcp4.washington.edu>
2019-08-05 12:46:19 -0700

    added new workflow

---
+++ new_scrape.py
@@ -0,0 +1,16 @@
+# Start with 10 words
+
+# for each word
+# search, grab top 20 posts, choose 'most recent' grab 20 more
+  # for each post, grab:
+    # username
+    # user title
+    # user description
+    # user avatar
+    # post text
+    # post tags
+    # post # notes
+    # likers
+    # rebloggers
+    # reblogged from
+    
